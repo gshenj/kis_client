95aac92f45d6d0edc67c7e9b3a9e3c117cc686c2
gshenj <gshenj@gmail.com>
2019-12-29 12:16:50 +0800

    v1.2

--- build.js
+++ build.js
@@ -1,6 +1,7 @@
 var electronInstaller = require('electron-winstaller');
 var path = require("path");
 
+var now = new Date();
 resultPromise = electronInstaller.createWindowsInstaller({
     appDirectory: path.join('../build_out/kis-win32-ia32'), //刚才生成打包文件的路径
     outputDirectory: path.join('../build_out/'), //输出路径
@@ -9,7 +10,7 @@ resultPromise = electronInstaller.createWindowsInstaller({
     loadingGif:path.join('images/loadingInstall.gif'),
     authors: 'shenjin', // 作者名称
     exe: 'kis.exe', //在appDirectory寻找exe的名字;
-    setupExe:'kis20190410.exe',
+    setupExe:'kis-'+ now.getFullYear()+ (now.getMonth()+1) + now.getDate() + '.exe',
     noMsi: true
 });
 
