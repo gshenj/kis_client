252d07d47f2581d6b57148e546f1575b02dacaf0
gshenj <gshenj@gmail.com>
2020-01-06 16:17:09 +0800

    c

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ const path = require("path");
 const default_url = '';
 const dlgIcon = path.join(__dirname, 'app.ico');
 
+let appName = '更新提示';
+//console.log('check update');
+let message = {
+    error: '检查更新出错',
+    checking: '正在检查更新……',
+    updateAva: '发现新版本，是否更新？',
+    updateNotAva: '现在使用的就是最新版本，不用更新',
+    downloaded: '最新版本已下载，将在重启程序后更新'
+}
+
 
 // 保持对window对象的全局引用，如果不这么做的话，当JavaScript对象被
 // 垃圾回收的时候，window对象将会自动的关闭
@@ -67,25 +77,74 @@ function createWindow() {
     contents.loadURL(url);
 
 
+    ipcMain.on('check-update', (event, arg) => {
+        console.log(arg) // prints "ping"
+        const request = net.request(getUpdateUrl() + '/RELEASES?t=' + new Date().getTime())
+        request.on('response', (response) => {
+            console.log(`STATUS: ${response.statusCode}`)
+            console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
+            let txt = '';
+            response.on('data', (chunk) => {
+                txt += chunk.toString('utf8')
+            })
+
+            response.on('end', () => {
+                console.log('No more data in response.')
+                let vInfos = txt.split(' ')
+                let appNames = vInfos[1].split('-');
+                console.log(appNames[1])
+                var pjson = require('./package.json');
+                console.log(pjson.version)
+                if (versionCompare(appNames[1], pjson.version) > 0) {
+                    console.log('have')
+                    //tipUpdate();
+                    //document.getElementById('msg').innerHTML = "发现新版本，是否更新？"
+                    event.reply('check-update-reply', appNames[1])
+                } else {
+                    event.reply('check-update-reply', '')
+                }
+            })
+            response.on('error', () => {
+                console.log('err.')
+            })
+        })
+        request.end()
+    })
+
+
 
     // add tray
-
-    appIcon = new Tray(path.join(__dirname, 'app.ico'))
+    const appIcon = new Tray(path.join(__dirname, 'app.ico'))
     const contextMenu = Menu.buildFromTemplate([
-        { label: '检查更新', click() { console.log('check update'); checkForUpdates(); } },
-        { label: '关于', click() { 
-             //mainWindow.webContents.send('show_about')
-             const info = JSON.stringify(process.versions);
+    {
+        label: '检查更新',
+        click() {
+            ///getUpdates();
+            tipUpdate();
+            //  checkForUpdates();
+        }
+    },
+    {
+        label: '关于',
+        click() {
+            //mainWindow.webContents.send('show_about')
+            const info = JSON.stringify(process.versions);
             console.log(info);
-             dialog.showMessageBoxSync(mainWindow, {
+            dialog.showMessageBoxSync(mainWindow, {
                 type: 'info',
                 icon: dlgIcon,
                 buttons: ['OK'],
                 title: '关于',
                 message: info
             });
-         } }
-    ])
+        }
+    },
+    {
+        label: '退出',
+        click() {
+            mainWindow.close();
+        }
+    }, ])
 
     // Call this again for Linux because we modified the context menu
     appIcon.setContextMenu(contextMenu)
@@ -119,7 +178,7 @@ function getUrl() {
 
 function getUpdateUrl() {
     try {
-    return require('./kis.json').updateUrl;
+        return require('./kis.json').updateUrl;
     } catch (e) {
         console.error(e);
     }
@@ -136,17 +195,17 @@ function startupEventHandle() {
         }
         var squirrelCommand = process.argv[1];
         switch (squirrelCommand) {
-            case '--squirrel-install':
-            case '--squirrel-updated':
-                install();
-                return true;
-            case '--squirrel-uninstall':
-                uninstall();
-                app.quit();
-                return true;
-            case '--squirrel-obsolete':
-                app.quit();
-                return true;
+        case '--squirrel-install':
+        case '--squirrel-updated':
+            install();
+            return true;
+        case '--squirrel-uninstall':
+            uninstall();
+            app.quit();
+            return true;
+        case '--squirrel-obsolete':
+            app.quit();
+            return true;
         }
         // 安装
         function install() {
@@ -181,17 +240,7 @@ function updateHandle() {
 }
 
 function checkForUpdates() {
-    let appName = '更新提示';
-    console.log(__filename)
-    //let appIcon = path.join(__dirname, 'app.ico');
-    let message = {
-        error: '检查更新出错',
-        checking: '正在检查更新……',
-        updateAva: '发现新版本，是否更新？',
-        updateNotAva: '现在使用的就是最新版本，不用更新',
-        downloaded: '最新版本已下载，将在重启程序后更新'
-    };
-    
+
     const updateUrl = getUpdateUrl();
     if (!updateUrl) {
         console.log('No update url!')
@@ -210,51 +259,130 @@ function checkForUpdates() {
             detail: '\r' + error
         });
     })
-        .on('checking-for-update', function (e) {
-            return dialog.showMessageBoxSync(mainWindow, {
-                type: 'info',
-                icon: dlgIcon,
-                buttons: [],
-                title: appName,
-                message: message.checking
-            });
-        })
-        .on('update-available', function (e) {
-            var downloadConfirmation = dialog.showMessageBoxSync(mainWindow, {
-                type: 'info',
-                icon: dlgIcon,
-                buttons: ['OK'],
-                title: appName,
-                message: message.updateAva
-            });
-            if (downloadConfirmation === 0) {
+    autoUpdater.on('checking-for-update', function (e) {
+        // return dialog.showMessageBoxSync(mainWindow, {
+        //     type: 'info',
+        //     icon: dlgIcon,
+        //     buttons: [],
+        //     title: appName,
+        //     message: message.checking
+        // });
+    })
+    autoUpdater.on('update-available', function (e) {
+        // var downloadConfirmation = dialog.showMessageBoxSync(mainWindow, {
+        //     type: 'info',
+        //     icon: dlgIcon,
+        //     buttons: ['OK'],
+        //     title: appName,
+        //     message: message.updateAva
+        // });
+        // if (downloadConfirmation === 0) {
 
-                return true;
-            } else {
-                return false;
-            }
-        })
-        .on('update-not-available', function (e) {
-            return dialog.showMessageBoxSync(mainWindow, {
-                type: 'info',
-                icon: dlgIcon,
-                buttons: ['OK'],
-                title: appName,
-                message: message.updateNotAva
-            });
-        })
-        .on('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
-            var index = dialog.showMessageBoxSync(mainWindow, {
-                type: 'info',
-                icon: dlgIcon,
-                buttons: ['现在重启', '稍后重启'],
-                title: appName,
-                message: message.downloaded,
-                detail: releaseName + "\n\n" + releaseNotes
-            });
-            if (index === 1) return;
-            autoUpdater.quitAndInstall();
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+    })
+    autoUpdater.on('update-not-available', function (e) {
+        return dialog.showMessageBoxSync(mainWindow, {
+            type: 'info',
+            icon: dlgIcon,
+            buttons: ['OK'],
+            title: appName,
+            message: message.updateNotAva
         });
+    })
+    autoUpdater.on('update-downloaded', function (event, releaseNotes, releaseName, releaseDate, updateUrl, quitAndUpdate) {
+        var index = dialog.showMessageBoxSync(mainWindow, {
+            type: 'info',
+            icon: dlgIcon,
+            buttons: ['现在重启', '稍后重启'],
+            title: appName,
+            message: message.downloaded,
+            detail: releaseName + "\n\n" + releaseNotes
+        });
+        if (index === 1) return;
+        autoUpdater.quitAndInstall();
+    });
 
     autoUpdater.checkForUpdates();
+}
+
+
+
+/*
+function getUpdates() {
+    const { net } = require('electron')
+    const request = net.request(getUpdateUrl() + '/RELEASES?t=' + new Date().getTime())
+    request.on('response', (response) => {
+        console.log(`STATUS: ${response.statusCode}`)
+        console.log(`HEADERS: ${JSON.stringify(response.headers)}`)
+        let txt = '';
+        response.on('data', (chunk) => {
+            txt += chunk.toString('utf8')
+        })
+
+        response.on('end', () => {
+            console.log('No more data in response.')
+            let vInfos = txt.split(' ')
+            let appNames = vInfos[1].split('-');
+            console.log(appNames[1])
+            var pjson = require('./package.json');
+            console.log(pjson.version)
+            if (versionCompare(appNames[1], pjson.version) > 0) {
+                console.log('have')
+                tipUpdate();
+            } else {
+                console.log('no')
+            }
+
+        })
+        response.on('error', () => {
+            console.log('err.')
+        })
+    })
+    request.end()
+}
+
+*/
+
+function tipUpdate() {
+    let child = new BrowserWindow({
+        parent: mainWindow,
+        modal: true,
+        width: 400,
+        height: 300,
+        autoHideMenuBar: true,
+        show: false,
+        title: '系统更新'
+    })
+    child.loadFile('update.html')
+    child.once('ready-to-show', () => {
+        child.show()
+    })
+}
+
+
+function versionCompare(v1, v2) {
+    let arr1 = v1.split('.');
+    let arr2 = v2.split('.');
+    let i = 0;
+    for (;;) {
+        if (arr1[i] && arr2[i]) {
+            arr1[i] = parseInt(arr1[i])
+            arr2[i] = parseInt(arr2[i])
+            if (arr1[i] == arr2[i]) {
+                i++;
+                continue;
+            } else if (arr1[i] > arr2[i]) {
+                return 1;
+            } else {
+                return -1;
+            }
+
+        } else {
+            // 版本不一致，无法比较，或者相同
+            return 0
+        }
+    }
 }
